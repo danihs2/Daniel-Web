71207cc0566dcb346ec7f65d0ef0d86a8285e6a3
DanielSalas <daniel.hachac@alumno.buap.mx>
2024-03-13 00:30:45 -0600

    Mensaje de confirmación

--- first-next-project/app/layout.js
+++ first-next-project/app/layout.js
@@ -1,24 +1,22 @@
 import Link from "next/link" // Componente Link para navegación entre páginas
 import Navbar from "../components/Navbar"
-import {
-  Roboto
-} from "next/font/google"
+import { Poppins } from 'next/font/google'
+
 
 export const metadata = {
   title: 'Holita - Home',
   description: 'This is the home page',
 }
-
-const roboto = Roboto({
-  weights: ["300", "400", "500", "700"],
-  styles: ["italic", "normal"],
-  subsets: ["latin-ext"],
+ 
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin-ext'],
 })
 
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={poppins.className}>
         <Navbar />
 
         {children}
